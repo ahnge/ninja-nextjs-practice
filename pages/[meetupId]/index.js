ae625361d3732b1ef01c767cb17c60dc1b7d708e
Ahnge <ahnge226@gmail.com>
2022-01-28 20:11:51 +0630

    fixed fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -32,7 +32,7 @@ export const getStaticPaths = async () => {
   client.close();
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: data.map((meetup) => ({
       params: {
         meetupId: meetup._id.toString(),
